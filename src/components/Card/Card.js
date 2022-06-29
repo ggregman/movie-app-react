86a455c946bfe729a94fa0042af99e7664f47a84
Roberto <11253858+robrub@users.noreply.github.com>
2022-06-29 08:16:41 +0000

    Corretta proprietà key sulle Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,36 +1,37 @@
-import { Box, Image, Text } from '@chakra-ui/react';
-import { Modale } from './Modale.js';
+import { Box, Image, Text } from "@chakra-ui/react";
+import { Modale } from "./Modale.js";
 
 export const Card = (props) => {
+  const { currentMovie } = props;
 
-    const { currentMovie, numElemento } = props;
+  return (
+    <Box
+      borderWidth="1px"
+      borderInline="outset"
+      borderColor="white"
+      borderStyle="solid"
+      borderRadius="10px"
+      maxW="250px"
+      bgColor="white"
+      overflow="hidden"
+    >
+      <Image src={currentMovie.Poster} alt={currentMovie.Title} />
+      <Box padding="5">
+        <Text fontWeight="Bold" as="h2">
+          {currentMovie.Title}
+        </Text>
 
-    return (
+        <Text>{currentMovie.Year}</Text>
 
-        <Box borderWidth='1px' borderInline='outset' borderColor='white' borderStyle='solid' borderRadius='10px' maxW='250px' bgColor='white' overflow='hidden'>
-
-            <Image src={currentMovie.Poster} alt={currentMovie.Title} />
-
-            <Box padding='5'>
-
-                <Box color='gray.500' fontSize='xs' textTransform='uppercase'>
-                    {/* {currentMovie.Genre} | {currentMovie.runtime} */}
-                </Box>
-
-                <Text fontWeight='Bold' as='h2'>{currentMovie.Title}</Text>
-
-                <Text>{currentMovie.Year}</Text>
-
-                <Box display='flex' mt='2' alignItems='center' justifyContent='space-between'>
-                    <Modale />
-                    8.7
-                   <Text> {numElemento} </Text>
-                </Box>
-
-            </Box>
-
+        <Box
+          display="flex"
+          mt="2"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Modale />
         </Box>
-
-    )
-}
-
+      </Box>
+    </Box>
+  );
+};
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,28 +1,28 @@
-import { Center, SimpleGrid } from "@chakra-ui/react"
-import { getMovies } from "../api/getMovies.js"
-import { Card } from "./Card/Card.js"
+import { Center, SimpleGrid } from "@chakra-ui/react";
+import { getMovies } from "../api/getMovies.js";
+import { Card } from "./Card/Card.js";
 
 export const CardsContainer = () => {
+  const movies = getMovies();
 
-    const movies = getMovies();
-
-    return (
-        <>
-            <Center m="20px" color='white'>
-                Risultati (10)
-            </Center>
-
-            <Center>
-
-                <SimpleGrid maxW="80%" spacing='20px' marginTop="20px" marginBottom='50px' minChildWidth="200px" >
-
-                    {
-                        movies.map( (movie, Index) => <Card currentMovie={movie} numElemento={Index}/>)
-                    }
-
-                </SimpleGrid>
-
-            </Center>
-        </>
-    )
-}
+  return (
+    <>
+      <Center m="20px" color="white">
+        Risultati (10)
+      </Center>
+      <Center>
+        <SimpleGrid
+          maxW="80%"
+          spacing="20px"
+          marginTop="20px"
+          marginBottom="50px"
+          minChildWidth="200px"
+        >
+          {movies.map((movie, index) => (
+            <Card currentMovie={movie} key={index} />
+          ))}
+        </SimpleGrid>
+      </Center>
+    </>
+  );
+};
